4682c933a7d1a53e776b8ffd98ada96698e49d36
brar-hub <sharanbr18@gmail.com>
2026-08-24 09:34:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,24 @@ monthly_rent = st.number_input(
     value=3000
 )
 
+down_payment = st.number_input(
+    "Down Payment ($)",
+    min_value=0,
+    value=100000
+)
+
+interest_rate = st.number_input(
+    "Mortgage Interest Rate (%)",
+    min_value=0.0,
+    value=4.5,
+    step=0.1
+)
+
+amortization_years = st.number_input(
+    "Amortization (Years)",
+    min_value=1,
+    value=25
+)
 annual_rent = monthly_rent * 12
 
 st.header("Basic Analysis")
